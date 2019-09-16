f92dbedf3d5a998814124f44445a74c38cca366c
slawek paciorek <slawekpaciorek@gmail.com>
2019-09-17 00:09:29 +0200

    Add some fields to basic class, add loggers)

--- src/main/java/it/slawekpaciorek/model/UserOrder.java
+++ src/main/java/it/slawekpaciorek/model/UserOrder.java
@@ -1,12 +1,68 @@
 package it.slawekpaciorek.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 public class UserOrder {
+
+    Logger logger = LoggerFactory.getLogger(UserOrder.class);
 
     int userId;
     long requestId;
     String name;
     List<Product> productList;
 
+    public UserOrder(){
+        logger.info("Creating USER ORDER by user with id number : " + userId);
+    }
+
+    public UserOrder(int userId, String name, List<Product> productList) {
+        this.userId = userId;
+        this.name = name;
+        this.productList = productList;
+
+        logger.info("Creating USER ORDER by user with id number : " + userId);
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public void setLogger(Logger logger) {
+        this.logger = logger;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public long getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(long requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
 }
--- src/main/java/it/slawekpaciorek/repo/InMemoryDB.java
+++ src/main/java/it/slawekpaciorek/repo/InMemoryDB.java
@@ -1,4 +1,59 @@
 package it.slawekpaciorek.repo;
 
+import it.slawekpaciorek.model.User;
+import it.slawekpaciorek.model.UserOrder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InMemoryDB {
+
+    protected static Logger logger = LoggerFactory.getLogger(InMemoryDB.class);
+
+    private static Set<User> users;
+    private static List<UserOrder> orders;
+
+    public static Set<User> getUsers() {
+        if(users == null) {
+            logger.warn("Users list is not initialized, initializing list");
+            users = new HashSet<User>();
+        }
+        logger.debug("Accessing to users list");
+        return users;
+    }
+
+    public static void addUser(User user){
+        if(users == null) {
+            logger.warn("Users list is not initialized, initializing list");
+            users = new HashSet<User>();
+        }
+        logger.debug("Adding user : " + user.toString() + " to database.");
+        users.add(user);
+    }
+
+    public static List<UserOrder> getOrders() {
+        if(orders == null) {
+            logger.warn("Orders list is not initialized, initializing list");
+            orders = new ArrayList<>();
+        }
+
+        logger.debug("Accessing to orders list");
+        return orders;
+    }
+
+    public static void addOrder(UserOrder order){
+        if(orders == null) {
+            logger.warn("Orders list is not initialized, initializing list");
+            orders = new ArrayList<UserOrder>();
+        }
+
+        logger.debug("Adding order : " + order.toString() + " to database.");
+        orders.add(order);
+    }
+
+
 }
